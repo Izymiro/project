d5b4839f7d276f50a99b78d47c40c43e646459b4
Izymiro <example@gmail.com>
2020-11-20 17:48:16 +0600

    15 урок практика (2)

--- js/script.js
+++ js/script.js
@@ -2,6 +2,16 @@
 
 let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
+if (numberOfFilms.length <= 0) {
+	alert('Пожалуйста введите что то');
+	let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+} else if (numberOfFilms.length >= 50) {
+	alert('Пожалуйста введите менее 50 символов');
+	let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+} else {
+	alert('Спасибо за ответ!');
+};
+
 let personalMovieDB = {
 	count: numberOfFilms,
 	movies: {},
@@ -11,11 +21,22 @@ let personalMovieDB = {
 
 
 };
-//console.log(personalMovieDB.count);
+console.log(personalMovieDB['count']);
+if (personalMovieDB['count'] < 10) {
+	alert('Просмотренно довольно мало фильмов');
+} else if (personalMovieDB['count'] >= 10 && personalMovieDB['count'] <= 30 ) {
+	alert('Вы классик зритель');
+}else if (personalMovieDB['count'] >= 30) {
+	alert('Вы киноман');
+}else {
+	alert('Произошла ошибка');
+}
 
-let seeMovies = prompt('Один из последних просмотренных фильмов?', '');
-let raitingMovies = prompt('На сколько оцените его?', '');
+//let seeMovies = prompt('Один из последних просмотренных фильмов?', '');
+//let raitingMovies = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[seeMovies] = raitingMovies;
+//personalMovieDB.movies[seeMovies] = raitingMovies;
 
-console.log(personalMovieDB);
+
+
+//console.log(personalMovieDB);
