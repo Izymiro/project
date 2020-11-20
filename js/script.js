439a41cb6fb5eeb33986fa9830b017347ac44d52
Izymiro <example@gmail.com>
2020-11-20 20:37:10 +0600

    15 Yrok 2 practice

--- js/script.js
+++ js/script.js
@@ -1,42 +1,35 @@
 'use strict';
 
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-if (numberOfFilms.length <= 0) {
-	alert('Пожалуйста введите что то');
-	let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-} else if (numberOfFilms.length >= 50) {
-	alert('Пожалуйста введите менее 50 символов');
-	let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-} else {
-	alert('Спасибо за ответ!');
-};
-
-let personalMovieDB = {
+const personalMovieDB = {
 	count: numberOfFilms,
 	movies: {},
 	actors: {},
 	genres: [],
 	privat: false,
-
-
 };
-console.log(personalMovieDB['count']);
-if (personalMovieDB['count'] < 10) {
-	alert('Просмотренно довольно мало фильмов');
-} else if (personalMovieDB['count'] >= 10 && personalMovieDB['count'] <= 30 ) {
-	alert('Вы классик зритель');
-}else if (personalMovieDB['count'] >= 30) {
-	alert('Вы киноман');
-}else {
-	alert('Произошла ошибка');
+
+for (let i = 0; i < 2; i++) {
+	const a = prompt('Один из последних просмотренных фильмов?', ''),
+			b = prompt('На сколько оцените его?', '');
+
+	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+		personalMovieDB.movies[a] = b;
+	} else {
+		console.log('error');
+		i--;
+	}
 }
 
-//let seeMovies = prompt('Один из последних просмотренных фильмов?', '');
-//let raitingMovies = prompt('На сколько оцените его?', '');
+if (personalMovieDB.count < 10) {
+	console.log('Просмотренно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	console.log('Вы классик зритель');
+} else if (personalMovieDB.count >= 30) {
+	console.log('Вы киношник');
+} else {
+	console.log('Произошла ошибка');
+}
 
-//personalMovieDB.movies[seeMovies] = raitingMovies;
-
-
-
-//console.log(personalMovieDB);
+console.log(personalMovieDB);
